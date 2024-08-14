970373606e5284f2ecdc3d805b31cc5d57bfdd48
JoselitoJP <joao21pauloti@gmail.com>
2024-08-14 15:33:51 -0300

    dart_2°aula_modulo1°_Dart:POO

--- Dart/projeto_curso_dois/projeto_curso_dois/bin/projeto_curso_dois.dart
+++
@@ -1,5 +0,0 @@
-import 'package:projeto_curso_dois/projeto_curso_dois.dart' as projeto_curso_dois;
-
-void main(List<String> arguments) {
-  print('Hello world: ${projeto_curso_dois.calculate()}!');
-}
