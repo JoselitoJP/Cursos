94ff45d330c98502f7c3486adcababc41a90a707
JoselitoJP <joao21pauloti@gmail.com>
2024-08-16 11:45:56 -0300

    3°aula_Modulo2_DartPOO_16_08_24

--- Dart/projeto_curso_dois/projeto_curso_dois/bin/dart_curso_dois.dart
+++ Dart/projeto_curso_dois/projeto_curso_dois/bin/dart_curso_dois.dart
@@ -1,17 +1,21 @@
 void main() {
   print("Ola, Mundo! Você esta feliz? ");
-
+//03º aula do Modulo 02 Dart:entenda a Orientção a Objeto
   String nome = "Laranja";
   double peso = 100.2;
   String cor = "Verde e Amarela";
   String sabor = "Doce e Cítrica";
-  int diasDesdeColheita = 40;
-  bool isMadura;
+  int diasDesdeColheita = 20;
+  bool isMadura = funcEstaMadura(diasDesdeColheita);
 
-  if(diasDesdeColheita >= 30){
-    isMadura = true;
+  print(isMadura);
+}
+
+bool funcEstaMadura(int dias){
+  if(dias >= 30){
+    return true;
   }else{
-    isMadura = false;
+    return false;
   }
-  
+
 }
