e15e9471a359cb425869dd046026184083050b2f
JoselitoJP <joao21pauloti@gmail.com>
2024-08-16 10:51:21 -0300

    dart

--- Dart/projeto_curso_dois/projeto_curso_dois/bin/dart_curso_dois.dart
+++ Dart/projeto_curso_dois/projeto_curso_dois/bin/dart_curso_dois.dart
@@ -1,6 +1,17 @@
 void main() {
   print("Ola, Mundo! Você esta feliz? ");
 
-  String nome = 'Laranja';
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e Cítrica";
+  int diasDesdeColheita = 40;
+  bool isMadura;
+
+  if(diasDesdeColheita >= 30){
+    isMadura = true;
+  }else{
+    isMadura = false;
+  }
   
 }
