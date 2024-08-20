bf478dbca6f7944682719293755d8c660b4d12b3
JoselitoJP <joao21pauloti@gmail.com>
2024-08-20 14:30:44 -0300

    07°aula_Modulo02_Dart:POO

--- Dart/projeto_curso_dois/bin/dart_curso_dois.dart
+++ Dart/projeto_curso_dois/bin/dart_curso_dois.dart
@@ -5,30 +5,36 @@ void main() {
   double peso = 100.2;
   String cor = "Verde e Amarela";
   String sabor = "Doce e Cítrica";
-  int diasDesdeColheita = 20;
+  int diasDesdeColheita = 40;
   bool isMadura = funcEstaMadura(diasDesdeColheita);
 
   print(isMadura);
 
   mostrarMadura("Uva", 40, cor: "roxa");
+  int quantosDias = funcQuantosDiasMadura(diasDesdeColheita);
+  print(quantosDias);
 }
 
+ int funcQuantosDiasMadura(int dias){
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = diasParaMadura - dias;
+  return quantosDiasFaltam;
+}
+
+mostrarMadura(String nome, int dias, {required String cor/*="Semcor"*/}){ //void estaMadura({String? cor}){}
+  //05º aula do Modulo 02 Dart:entenda a Orientção a Objeto
+
+  if (dias >= 30) {
+    print("A $nome está madura!");
+  } else {
+    print("A $nome não está madura!");
+  }
 /*
   1 - Posicionais Obrigatórios
   2 - Nomeados Opcionais 
   3 - Parâmetros "Padrão"
   4 - Modificador "Required" 
 */
-
-//05º aula do Modulo 02 Dart:entenda a Orientção a Objeto
-
-mostrarMadura(String nome, int dias, {required String cor/*="Semcor"*/}){ //void estaMadura({String? cor}){}
-  if (dias >= 30) {
-    print("A $nome está madura!");
-  } else {
-    print("A $nome não está madura!");
-  }
-
   /*if (cor != null) {
     print("A $nome é $cor");
   }*/
