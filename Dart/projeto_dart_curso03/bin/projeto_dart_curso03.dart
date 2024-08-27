56c12ea369ffe68b8368e527afb7348c1fcc7442
JoselitoJP <joao21pauloti@gmail.com>
2024-08-27 15:51:17 -0300

    07ºaula_Modulo01_Dart:S. C. D.

--- Dart/projeto_dart_curso03/bin/projeto_dart_curso03.dart
+++ Dart/projeto_dart_curso03/bin/projeto_dart_curso03.dart
@@ -1,5 +1,37 @@
-import 'package:projeto_dart_curso03/projeto_dart_curso03.dart' as projeto_dart_curso03;
 
-void main(List<String> arguments) {
-  print('Hello world: ${projeto_dart_curso03.calculate()}!');
+void main(){
+  //07 aula Modulo 01 do curso de Dart:Sintaxe, Coleção, Dinamismo
+  //escolherMeioTransporte(0); 
+  escolherMeioTransporteEnum(Transporte.carro);
+}
+
+void escolherMeioTransporte(int locomocao){
+  if (locomocao == 0) {
+    print('Vou de CARRO para Aventura!');
+  } else if(locomocao == 1){
+    print('Vou de BIKE para Aventura!');
+  }else{
+    print('Vou para a Aventura');
+  }
+}
+
+void escolherMeioTransporteEnum(Transporte locomocao){
+  if (locomocao == Transporte.carro) {
+    print('Vou de CARRO para Aventura!');
+  } else if(locomocao == Transporte.bike){
+    print('Vou de BIKE para Aventura!');
+  }else {
+    print('Vou para a Aventura');
+  }
+}
+
+
+enum Transporte{
+  carro,
+  bike,
+  andando,
+  skate,
+  aviao,
+  patins,
+  trem,
 }
