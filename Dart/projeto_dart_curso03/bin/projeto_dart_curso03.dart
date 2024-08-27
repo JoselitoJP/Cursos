c338872601a36d75f77892dc88afc8632fc1b76d
JoselitoJP <joao21pauloti@gmail.com>
2024-08-27 15:18:13 -0300

    projeto_dart_curso03_27/08/24

---
+++ Dart/projeto_dart_curso03/bin/projeto_dart_curso03.dart
@@ -0,0 +1,5 @@
+import 'package:projeto_dart_curso03/projeto_dart_curso03.dart' as projeto_dart_curso03;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto_dart_curso03.calculate()}!');
+}
