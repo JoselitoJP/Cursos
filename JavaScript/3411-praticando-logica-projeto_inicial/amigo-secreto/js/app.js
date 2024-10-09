6e1e3e620986ffa7331d322bd618ff3d83bfb31f
JoãoPaulo <138796377+JoselitoJP@users.noreply.github.com>
2024-10-09 14:15:09 -0300

    Add files via upload

--- JavaScript/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ JavaScript/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -18,16 +18,42 @@ function adicionar(){
 	amigo.value = '';
 }
 
-function sortear(){
-	embaralha(amigos);
+function sortear() {
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = ''; // Limpa a lista antes de sortear
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i === amigos.length - 1) { // Último amigo
+            sorteio.innerHTML += amigos[i] + '-->' + amigos[0] + '<br>'; // Volta ao primeiro
+        } else {
+            sorteio.innerHTML += amigos[i] + '-->' + amigos[i + 1] + '<br>'; // Próximo amigo
+        }
+    }
 }
 
-function embaralha(){
+/*
+function sortear(){
+	embaralha(amigos);
+	let sorteio = document.getElementById('lista-soteio');
+	
+	
+	for (let i = 0; i < amigos.length; i++) {
+		console.log('foi');
+
+		if (i === amigos.length) {
+			sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
+		}else{
+			sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>';
+		}
+	}
+}*/
+
+function embaralha(lista){
 	for (let indice = lista.length; indice; indice--) {
 		const indiceAleatorio = Math.floor(Math.random() * indice);
 
 		//atribuição via destructuring
-		[lista[indice - 1], lista[indiceAleatorio]] = 
-			[lista[indiceAleatorio], lista[indice - 1]];
+		[lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
 	}
 }
