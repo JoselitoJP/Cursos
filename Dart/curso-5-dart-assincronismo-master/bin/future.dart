8809b3a28f6a24505c8ad3363571ed436ff74de9
JoselitoJP <joao21pauloti@gmail.com>
2025-01-02 15:59:34 -0300

    02°aula_Modulo02_Dart:EntendendoAssincronismo

---
+++ Dart/curso-5-dart-assincronismo-master/bin/future.dart
@@ -0,0 +1,9 @@
+void main(){
+
+  Future myfuture = Future((){
+    print('Going back to the Future');
+    return 21;
+  }).then((value) => print('The value is $value'));
+
+  print('Done with main');
+}
