fb136129b8ed8a74561ff4799f35749f238b8e07
JoselitoJP <joao21pauloti@gmail.com>
2025-01-04 16:21:55 -0300

    03°aula _Modulo02_Dart:EntendendoAssincronismo

--- Dart/curso-5-dart-assincronismo-master/bin/future.dart
+++ Dart/curso-5-dart-assincronismo-master/bin/future.dart
@@ -1,17 +1,38 @@
 void main(){
 
-  Future myfuture = Future((){
-    print('Going back to the Future');
-    return 21;
-  }).then((value) => print('The value is $value'));
+  // Future myfuture = Future((){
+  //   print('Going back to the Future');
+  //   return 21;
+  // }).then((value) => print('The value is $value'));
 
   Future <int> myFutureFunc()async{
     print('I have a function in the Future');
     await Future.delayed(Duration(seconds: 5));
+    throw Exception();
     return 12 ;
   }
 
-  myFutureFunc().then((value) => print('My function value is: $value'));
+  // myFutureFunc()
+  // .then((value) => print('My function value is: $value'))
+  // .onError((error, stackTrace) => print('An Error occurred'))
+  // .whenComplete(() => print('The Future is Over'));
 
+  Future<int> myFutureErrorFunc(int a, int b) async{
+    try {
+      if(a>b){
+        throw Exception();
+      }
+      print('I have a functional Function');
+      await Future.delayed(Duration(seconds: 5));
+      return 42; 
+    } catch (e) {
+      print('An error occourred: $e');
+      return 42;
+    }finally{
+      print('The Future is Over');
+    }
+  }
+
+  myFutureErrorFunc(2, 1).then((value) => print('The value is: $value'));
   print('Done with main');
 }
